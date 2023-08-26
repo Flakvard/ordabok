a0f1d21d65c89a2d7d7c57a50a8bd90ab159aa01
flakvard <marnifalkvard@gmail.com>
2023-08-26 11:39:34 +0200

    works

--- scrape_dict.py
+++ scrape_dict.py
@@ -1,15 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Base URL and parameters
-base_url = "URL_OF_THE_DICTIONARY_WEBSITE"
-search_terms = "abcdefghijklmnopqrstuvwxyz"
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-def scrape_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return None
+
+# Base URL and parameters
+
+# FO-FO = _DictionaryId=1
+
+# FO-EN = _DictionaryId=2
+#base_url = "https://sprotin.fo/dictionaries?_SearchInflections=0&_SearchDescriptions=0&_DictionaryId=2&_DictionaryPage=1&_SearchFor="
+
+# EN-FO = _DictionaryId=3
+base_url = "https://sprotin.fo/dictionaries?_SearchInflections=0&_SearchDescriptions=0&_DictionaryId=3&_DictionaryPage=1&_SearchFor="
+search_terms = "aábdðcefghiíjklmnoópqrstuúvyýæøwxz"
+#search_terms = "a"
+
+end_search = "&_l=fo&_Group="
+
+
+def scrape_page(driver, search_term):
+    url = f"{base_url}{search_term}{end_search}"
+    driver.get(url)
+    time.sleep(2)  # Give the page time to load
+
+    return driver.page_source
 
 def parse_page(html):
     soup = BeautifulSoup(html, "html.parser")
@@ -21,15 +40,25 @@ def parse_page(html):
             description = word.find("div", class_="dictionary-results--word-description").get_text(strip=True)
             yield title, description
 
-def main():
-    for term in search_terms:
-        url = f"{base_url}?SearchFor={term}"
-        html = scrape_page(url)
-        if html:
-            entries = parse_page(html)
-            with open(f"{term}_words.tsv", "w", encoding="utf-8") as file:
-                for title, description in entries:
-                    file.write(f"{title}\t{description}\n")
 
-if __name__ == "__main__":
-    main()
+#for headless browser use this arguments
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--window-size=1920x1080")
+driver = webdriver.Chrome(options=options)
+
+# Starts up chrome
+#driver = webdriver.Chrome()  
+
+search_terms = "aábdðcefghiíjklmnoópqrstuúvyýæøwxz"
+#search_terms = "a"
+
+try:
+    for term in search_terms:
+        html = scrape_page(driver, term)
+        entries = parse_page(html)
+        with open(f"{term}_words.tsv", "w", encoding="utf-8") as file:
+            for title, description in entries:
+                file.write(f"{title}\t{description}\n")
+finally:
+    driver.quit()
